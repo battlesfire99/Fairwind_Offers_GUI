fa8717de0f7ea2696c99b8a99b4f13bbb5852204
battlesfire99 <64383967+battlesfire99@users.noreply.github.com>
2022-01-13 10:39:57 +0100

    GUI v1.0 finished

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import QtWidgets, uic
+import resources
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = QtWidgets.QApplication(sys.argv)
 
-import PyQt5
+window = uic.loadUi("gui.ui")
+window.show()
+app.exec_()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('hahah')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
